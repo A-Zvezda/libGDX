43d6c752556654ab5a17f894d4ec8e2d40263b86
Aleksandr Zvezda <au.zvezda@gmail.com>
2019-11-14 00:56:43 +0600

    Lesson 04

--- StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
+++ StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,78 +1,107 @@
 package ru.geekbrains.screen;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
-import ru.geekbrains.sprite.Logo;
+import ru.geekbrains.sprite.ButtonExit;
+import ru.geekbrains.sprite.ButtonPlay;
+import ru.geekbrains.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture img;
-    private Texture bg;
-    private Logo logo;
-    private Background background;
+    private static final int STAR_COUNT = 256;
 
+    private Game game;
+
+    private Texture bg;
+    private TextureAtlas atlas;
+
+    private Background background;
+    private Star[] stars;
+    private ButtonExit buttonExit;
+    private ButtonPlay buttonPlay;
+
+    public MenuScreen(Game game) {
+        this.game = game;
+    }
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
         bg = new Texture("textures/bg.png");
-        logo = new Logo(new TextureRegion(img));
-        logo.setHeightProportion(0.5f);
         background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        stars = new Star[256];
+        for (int i = 0; i < STAR_COUNT; i++) {
+            stars[i] = new Star(atlas);
+        }
+        buttonExit = new ButtonExit(atlas);
+        buttonPlay = new ButtonPlay(atlas, game);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        background.draw(batch);
-        logo.draw(batch);
-        logo.update(0.01f);
-        batch.end();
+        update(delta);
+        draw();
     }
 
     @Override
     public void dispose() {
-        img.dispose();
         bg.dispose();
+        atlas.dispose();
         super.dispose();
     }
-    @Override
-    public boolean keyDown(int keycode) {
-        logo.keyDown(keycode);
-        return false;
-    }
 
-    @Override
-    public boolean keyUp(int keycode) {
-        logo.keyUp(keycode);
-        return false;
-    }
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        buttonExit.resize(worldBounds);
+        buttonPlay.resize(worldBounds);
     }
+
     @Override
     public boolean touchDown(Vector2 touch, int pointer) {
-        logo.touchDown(touch, pointer);
+        buttonExit.touchDown(touch, pointer);
+        buttonPlay.touchDown(touch, pointer);
         return false;
     }
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer) {
-        logo.touchUp(touch, pointer);
+        buttonExit.touchUp(touch, pointer);
+        buttonPlay.touchUp(touch, pointer);
         return false;
     }
 
+    private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
+        }
+    }
+
+    private void draw() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        buttonExit.draw(batch);
+        buttonPlay.draw(batch);
+        batch.end();
+    }
 }
