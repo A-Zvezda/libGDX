72040213cf5f56a0b8beea7bb37b4ac1ac989390
Aleksandr Zvezda <au.zvezda@gmail.com>
2019-11-18 13:43:06 +0600

    ready

---
+++ StarGame/core/src/ru/geekbrains/screen/GameScreen.java
@@ -0,0 +1,120 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.pool.BulletPool;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.MainShip;
+import ru.geekbrains.sprite.Star;
+
+public class GameScreen extends BaseScreen {
+
+    private static final int STAR_COUNT = 64;
+
+    private Texture bg;
+    private TextureAtlas atlas;
+
+    private Background background;
+    private Star[] stars;
+    private MainShip mainShip;
+    private Music music;
+    private BulletPool bulletPool;
+
+    @Override
+    public void show() {
+        super.show();
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < STAR_COUNT; i++) {
+            stars[i] = new Star(atlas);
+        }
+        bulletPool = new BulletPool();
+        mainShip = new MainShip(atlas, bulletPool);
+        this.music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+        this.music.setLooping(true);
+        this.music.play();
+
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+        freeAllDestroyed();
+        draw();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        mainShip.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        bg.dispose();
+        music.dispose();
+        atlas.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        mainShip.touchDown(touch, pointer);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+        mainShip.touchUp(touch, pointer);
+        return false;
+    }
+
+    private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
+        }
+        mainShip.update(delta);
+        bulletPool.updateActiveSprites(delta);
+    }
+
+    private void freeAllDestroyed() {
+        bulletPool.freeAllDestroyedActiveSprites();
+    }
+
+    private void draw() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        mainShip.draw(batch);
+        bulletPool.drawActiveSprites(batch);
+        batch.end();
+    }
+}
--- StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
+++ StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,149 +1,113 @@
 package ru.geekbrains.screen;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.ButtonExit;
+import ru.geekbrains.sprite.ButtonPlay;
+import ru.geekbrains.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture texture;
-    private Texture starShip;
-    private Vector2 currentPos;
-    private Vector2 velocity;
-    private Vector2 destinationPoint;
+    private static final int STAR_COUNT = 256;
+
+    private Game game;
+
+    private Texture bg;
+    private TextureAtlas atlas;
+
+    private Background background;
+    private Star[] stars;
+    private ButtonExit buttonExit;
+    private ButtonPlay buttonPlay;
+    private Music music;
+    public MenuScreen(Game game) {
+        this.game = game;
+    }
 
     @Override
     public void show() {
         super.show();
-        texture = new Texture("space.jpg");
-        starShip = new Texture( "whiteStar128x64.png");
-        //starShip.w
-        currentPos = new Vector2();
-        velocity = new Vector2(0,0);
-        destinationPoint = new Vector2(0,0);
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < STAR_COUNT; i++) {
+            stars[i] = new Star(atlas);
+        }
+        buttonExit = new ButtonExit(atlas);
+        buttonPlay = new ButtonPlay(atlas, game);
+
+        this.music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+        this.music.setLooping(true);
+        this.music.play();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(texture, 0, 0);
-        batch.draw(starShip, currentPos.x, currentPos.y, 128, 64);
-        batch.end();
-        if (Gdx.graphics.getHeight() >= currentPos.y + starShip.getHeight()
-            && Gdx.graphics.getWidth() >= currentPos.x + starShip.getWidth()
-                && currentPos.x >= 0 && currentPos.y >= 0) {
-            if (velocity.x != 0 ||  velocity.y != 0) {
-                currentPos.add(velocity);
-
-            if (currentPos.x == destinationPoint.x  ) {
-                velocity.x = 0;
-            }
-            if (currentPos.y == destinationPoint.y) {
-                velocity.y = 0;
-            }
-
-                if (currentPos.x == destinationPoint.x && currentPos.y == destinationPoint.y ) {
-                    velocity.set (0, 0);
-                }
-            if (Gdx.graphics.getHeight() <= currentPos.y + starShip.getHeight()) {
-                currentPos.y = Gdx.graphics.getHeight()  - starShip.getHeight();
-                velocity.y = 0;
-            }
-            if (Gdx.graphics.getWidth() <= currentPos.x + starShip.getWidth()) {
-                currentPos.x =  Gdx.graphics.getWidth() - starShip.getWidth();
-                velocity.x = 0;
-            }
-            if (currentPos.x < 0 ) {
-                currentPos.x = 0;
-                velocity.x = 0;
-            }
-            if (currentPos.y < 0 ) {
-                currentPos.y = 0;
-                velocity.y = 0;
-            }
-            }
-
-        }
+        update(delta);
+        draw();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        texture.dispose();
+        bg.dispose();
+        atlas.dispose();
+        music.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        super.touchDown(screenX, screenY, pointer, button);
-        System.out.println(screenX + "; " + (Gdx.graphics.getHeight() - screenY));
-        destinationPoint.set(Math.round(screenX) - starShip.getWidth()/2, Math.round(Gdx.graphics.getHeight() - screenY) - starShip.getHeight()/2);
-        velocity = currentPos.cpy();
-        velocity.sub(destinationPoint);
-        velocity.scl(-1);
-        velocity.nor();
-        if (velocity.x < 0 ) {
-            velocity.x = -1;
-        } else if (velocity.x > 0 ) {
-            velocity.x = 1;
-        } else {
-            velocity.x = 0;
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
         }
-        if (velocity.y < 0 ) {
-            velocity.y = -1;
-        } else if (velocity.y > 0 ) {
-            velocity.y = 1;
-        } else {
-            velocity.y = 0;
-        }
-       // velocity.y = Math.round(velocity.y);
-        System.out.println( "velocity " + velocity.x + "; " + velocity.y);
-        System.out.println( "CP " + currentPos.x + "; " + currentPos.y);
+        buttonExit.resize(worldBounds);
+        buttonPlay.resize(worldBounds);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        buttonExit.touchDown(touch, pointer);
+        buttonPlay.touchDown(touch, pointer);
         return false;
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-        super.keyDown(keycode);
-        if (keycode == 19) {
-            velocity.set(0,1) ;
-        }
-        if (keycode == 20) {
-            velocity.set(0,-1) ;
-        }
-        if (keycode == 21) {
-            velocity.set(-1,0) ;
-        }
-        if (keycode == 22) {
-            velocity.set(1,0) ;
-        }
-
+    public boolean touchUp(Vector2 touch, int pointer) {
+        buttonExit.touchUp(touch, pointer);
+        buttonPlay.touchUp(touch, pointer);
         return false;
-
     }
 
-    @Override
-    public boolean keyUp(int keycode) {
-        super.keyUp(keycode);
-        if (keycode == 19) {
-            velocity.set(0,0) ;
+    private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
         }
-        if (keycode == 20) {
-            velocity.set(0,0) ;
+    }
+
+    private void draw() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
         }
-        if (keycode == 21) {
-            velocity.set(0,0) ;
-        }
-        if (keycode == 22) {
-            velocity.set(0,0) ;
-        }
-        return false;
+        buttonExit.draw(batch);
+        buttonPlay.draw(batch);
+        batch.end();
     }
 }
